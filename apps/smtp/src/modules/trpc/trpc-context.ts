607954be34b7b251ed6d2a2725ea2e1179bfdea5
ShashindaE <eshan981023@gmail.com>
2026-01-27 10:44:13 +0530

    Cleanup: Remove debug logs from TRPC context

--- apps/smtp/src/modules/trpc/trpc-context.ts
+++ apps/smtp/src/modules/trpc/trpc-context.ts
@@ -10,14 +10,6 @@ export const createTrpcContext = async ({ res, req }: trpcNext.CreateNextContext
   // CORRECTION: Map "pmtraders-" headers to "saleor-" headers so TRPC can authenticate
   const saleorApiUrl = (req.headers[SALEOR_API_URL_HEADER] || req.headers["pmtraders-api-url"]) as string | undefined;
 
-  /* eslint-disable no-console */
-  console.log("--- TRPC CONTEXT DEBUG ---");
-  console.log("Incoming Headers keys:", Object.keys(req.headers));
-  console.log("pmtraders-api-url:", req.headers["pmtraders-api-url"]);
-  console.log("Resolved saleorApiUrl:", saleorApiUrl);
-  console.log("Authorization Header:", req.headers[SALEOR_AUTHORIZATION_BEARER_HEADER]);
-
-
   return {
     token: req.headers[SALEOR_AUTHORIZATION_BEARER_HEADER] as string | undefined,
     saleorApiUrl: saleorApiUrl,
