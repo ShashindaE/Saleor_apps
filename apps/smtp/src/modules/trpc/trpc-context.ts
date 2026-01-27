3ae1d668c0745e96da1424b4e7ad2fec0173719a
ShashindaE <eshan981023@gmail.com>
2026-01-27 10:38:32 +0530

    Debug: Add logs to TRPC context to diagnose auth mismatch

--- apps/smtp/src/modules/trpc/trpc-context.ts
+++ apps/smtp/src/modules/trpc/trpc-context.ts
@@ -9,11 +9,14 @@ export const createTrpcContext = async ({ res, req }: trpcNext.CreateNextContext
 
   // CORRECTION: Map "pmtraders-" headers to "saleor-" headers so TRPC can authenticate
   const saleorApiUrl = (req.headers[SALEOR_API_URL_HEADER] || req.headers["pmtraders-api-url"]) as string | undefined;
-  // Saleor SDK usually relies on `saleor-api-url` but might check others.
-  // The token is "saleor-authorization-bearer" usually, or just "authorization-bearer" depending on SDK version?
-  // Actually, look at line 1: SALEOR_AUTHORIZATION_BEARER_HEADER
-  // If the backend also renames the auth header, we might need to map that too.
-  // But usually only the domain/url headers are white-labeled.
+
+  /* eslint-disable no-console */
+  console.log("--- TRPC CONTEXT DEBUG ---");
+  console.log("Incoming Headers keys:", Object.keys(req.headers));
+  console.log("pmtraders-api-url:", req.headers["pmtraders-api-url"]);
+  console.log("Resolved saleorApiUrl:", saleorApiUrl);
+  console.log("Authorization Header:", req.headers[SALEOR_AUTHORIZATION_BEARER_HEADER]);
+
 
   return {
     token: req.headers[SALEOR_AUTHORIZATION_BEARER_HEADER] as string | undefined,
